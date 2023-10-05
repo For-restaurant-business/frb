3f008fc7bc94f2204fae89e2fc566a9eb0065dcf
alexandr <alexander.mischenko@bask.ws>
2023-10-05 19:52:49 +0500

    Add: preline

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
+"use client;";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <main>
-      main page
-    </main>
-  )
+  return <main>main page</main>;
 }
