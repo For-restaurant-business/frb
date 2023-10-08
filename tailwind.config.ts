127e8892d3e66945818109cbcdc2f2024175fcb1
alexandr <alexander.mischenko@bask.ws>
2023-10-08 17:50:38 +0500

    linter fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'node_modules/preline/dist/*.js',
-	],
-	plugins: [require('preline/plugin')],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/preline/dist/*.js",
+  ],
+  plugins: [require("preline/plugin")],
 };
 export default config;
