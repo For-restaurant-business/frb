d95e4cd9a8a1d1d4b91f9e3c02da21adc9ce3e0d
alexandr <alexander.mischenko@bask.ws>
2023-10-23 00:47:34 +0500

    Add active menu item styles

--- components/Sidebar/Menus/index.tsx
+++ components/Sidebar/Menus/index.tsx
@@ -5,10 +5,15 @@ import { Menu } from "lib/types";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import MenusSkeleton from "./MenusSkeleton";
+import { usePathname } from "next/navigation";
 
 const Menus = () => {
   const [menus, setMenus] = useState<Menu[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const pathname = usePathname();
+  const urlMenuId = pathname.split("/")[2];
+  const isMenuActive = (menuId: string) => urlMenuId === menuId;
 
   useEffect(() => {
     getMenus()
@@ -24,7 +29,9 @@ const Menus = () => {
         menus.map((menu) => (
           <li
             key={menu.id}
-            className="flex items-center gap-x-3.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300"
+            className={`flex items-center gap-x-3.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 ${
+              isMenuActive(menu.id) ? "text-slate-400 dark:text-slate-200" : ""
+            }`}
           >
             <Link className="w-full py-2 px-2.5" href={`/menu/${menu.id}`}>
               {menu.name}
