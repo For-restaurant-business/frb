9ea0597d6eeeae10be9fcdc63f91e39b37bf5074
alexandr <alexander.mischenko@bask.ws>
2023-10-24 16:39:14 +0500

    fix resets cookie in vercel

--- middleware.ts
+++ middleware.ts
@@ -16,18 +16,19 @@ export async function middleware(request: NextRequest) {
         pb.authStore.exportToCookie({ httpOnly: false }),
       );
     }
-  } else {
-    pb.authStore.clear();
   }
+  // else {
+  //   pb.authStore.clear();
+  // }
 
   try {
     pb.authStore.isValid && (await pb.collection("users").authRefresh());
   } catch (err) {
-    pb.authStore.clear();
-    response.headers.set(
-      "set-cookie",
-      pb.authStore.exportToCookie({ httpOnly: false }),
-    );
+    // pb.authStore.clear();
+    // response.headers.set(
+    //   "set-cookie",
+    //   pb.authStore.exportToCookie({ httpOnly: false }),
+    // );
   }
 
   if (!authCookie && !request.nextUrl.pathname.startsWith("/auth")) {
