4bc9776084d911a6da76a06dfa9eec844f7c453a
alexandr <alexander.mischenko@bask.ws>
2023-10-24 22:35:15 +0500

    fix lint errors

--- components/InfoBlock/InfoBlockPhotos/index.tsx
+++ components/InfoBlock/InfoBlockPhotos/index.tsx
@@ -12,7 +12,7 @@ const InfoBlockPhotos = ({ images }: InfoBlockPhotosProps) => {
       {images.length == 2 && (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 flex-wrap">
           {images.map((i) => (
-            <div className="h-60 sm:h-96 relative dark:text-white">
+            <div className="h-60 sm:h-96 relative dark:text-white" key={i}>
               <Image
                 className="w-full object-cover rounded-xl"
                 src={i}
@@ -27,7 +27,10 @@ const InfoBlockPhotos = ({ images }: InfoBlockPhotosProps) => {
       )}
       {images.length == 1 &&
         images.map((i) => (
-          <div className="h-[200px] sm:h-[250px] md:h-[300px] lg:h-[350px] xl:h-[400px] 2xl:h-[500px] relative dark:text-white">
+          <div
+            className="h-[200px] sm:h-[250px] md:h-[300px] lg:h-[350px] xl:h-[400px] 2xl:h-[500px] relative dark:text-white"
+            key={i}
+          >
             <Image
               className="w-full object-cover rounded-xl"
               src={i}
